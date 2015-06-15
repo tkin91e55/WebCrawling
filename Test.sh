cd847b504b629a7ca0f0c963d479bc3886dee7da
tkingless <unstashworld@gmail.com>
2015-06-15 22:32:13 +0800

    a progress

---
+++ Test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source csvParsing.sh
+
+echo ${myarr[*]}
+
+echo ${myarr[0]}
+
+echo "myarr first element: ${myarr[0]}"
+
+echo "myarr second element: ${myarr[1]}"
--- csvParsing.sh
+++ csvParsing.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 #myarr=$(tail -n+2 config.csv | awk-csv-parser --output-separator=',' | grep SearchCrit | awk -F"," '{print $2}') #this work
+declare -a myarr
+myarr=$(tail -n+2 config.csv | awk-csv-parser --output-separator=',' | grep SearchCrit | awk -F"," '{print $2}') #this work
